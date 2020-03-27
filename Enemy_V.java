789ea2308205796eccb0f604d771f825bb8725d7
Anthony Comanzo <33241429+acomanzo@users.noreply.github.com>
2020-03-27 19:32:24 -0400

    Update Enemy_V.java
    Made it more abstract

--- Enemy_V.java
+++ Enemy_V.java
@@ -7,10 +7,8 @@ import java.awt.*;
  */
 public class Enemy_V extends GameObject {
 
-    private int[] x_points;
-    private int[] y_points;
-    private final int HEIGHT = 100;
-    private final int WIDTH = 80;
+    private int HEIGHT;
+    private int WIDTH;
     private Ball ball;
     private int health;
     private boolean dead;
@@ -20,10 +18,11 @@ public class Enemy_V extends GameObject {
     private boolean hit_top;
     private boolean hit_bottom;
 
-    public Enemy_V(int x, int y, Ball ball) {
-        super(x, y);
-        x_points = new int[] {x, x, x + 50, x + 50, x, x, x + 100, x + 100};
-        y_points = new int[] {y, y + 20, y + 20, y + 60, y + 60, y + 80, y + 80, y};
+    public Enemy_V(int x, int y, int width, int height, Ball ball, Color color) {
+        super(x, y, 0, 1, color);
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        makeIntegerCoordinates();
         this.ball = ball;
         health = 100;
         dead = false;
@@ -32,7 +31,15 @@ public class Enemy_V extends GameObject {
         hit_left = false;
         hit_top = false;
         hit_bottom = false;
-        setVelY(1);
+    }
+
+    public void makeCoordinates() {
+
+    }
+
+    public void makeIntegerCoordinates() {
+        xInts = new int[] {x, x, x + 50, x + 50, x, x, x + 100, x + 100};
+        yInts = new int[] {y, y + 20, y + 20, y + 60, y + 60, y + 80, y + 80, y};
     }
 
     public void tick() {
@@ -46,117 +53,117 @@ public class Enemy_V extends GameObject {
             dead = true;
         }
 
-        y_points[0] += velY;
-        y_points[1] += velY;
-        y_points[2] += velY;
-        y_points[3] += velY;
-        y_points[4] += velY;
-        y_points[5] += velY;
-        y_points[6] += velY;
-        y_points[7] += velY;
+        yInts[0] += velY;
+        yInts[1] += velY;
+        yInts[2] += velY;
+        yInts[3] += velY;
+        yInts[4] += velY;
+        yInts[5] += velY;
+        yInts[6] += velY;
+        yInts[7] += velY;
 
         listenForCollision();
     }
 
     public void render(Graphics g) {
         if (dead == false) {
-            g.setColor(Color.BLACK);
-            g.fillPolygon(x_points, y_points, 8);
+            g.setColor(color);
+            g.fillPolygon(xInts, yInts, 8);
         }
 
         if (hit_right) {
             g.setColor(Color.RED);
-            g.drawLine(x_points[7], y_points[7], x_points[6], y_points[6]);
-            g.drawLine(x_points[7], y_points[7] + 1, x_points[6], y_points[6] + 1);
+            g.drawLine(xInts[7], yInts[7], xInts[6], yInts[6]);
+            g.drawLine(xInts[7], yInts[7] + 1, xInts[6], yInts[6] + 1);
             hit_right = false;
         }
 
         if (hit_left) {
             g.setColor(Color.RED);
-            g.drawLine(x_points[0], y_points[0], x_points[1], y_points[1]);
-            g.drawLine(x_points[0], y_points[0] + 1, x_points[1], y_points[1] + 1);
+            g.drawLine(xInts[0], yInts[0], xInts[1], yInts[1]);
+            g.drawLine(xInts[0], yInts[0] + 1, xInts[1], yInts[1] + 1);
             hit_left = false;
         }
 
         if (hit_top) {
             g.setColor(Color.RED);
-            g.drawLine(x_points[0], y_points[0], x_points[7], y_points[7]);
-            g.drawLine(x_points[0], y_points[0] + 1, x_points[7], y_points[7] + 1);
+            g.drawLine(xInts[0], yInts[0], xInts[7], yInts[7]);
+            g.drawLine(xInts[0], yInts[0] + 1, xInts[7], yInts[7] + 1);
             hit_top = false;
         }
 
         if (hit_bottom) {
             g.setColor(Color.RED);
-            g.drawLine(x_points[1], y_points[1], x_points[7], y_points[7]);
-            g.drawLine(x_points[1], y_points[1] + 1, x_points[7], y_points[7] + 1);
+            g.drawLine(xInts[1], yInts[1], xInts[7], yInts[7]);
+            g.drawLine(xInts[1], yInts[1] + 1, xInts[7], yInts[7] + 1);
             hit_bottom = false;
         }
     }
 
     private void listenForCollision() {
-        double[] ball_x_points = ball.getxPoints();
-        double[] ball_y_points = ball.getyPoints();
+        double[] ball_xInts = ball.getXCoordinates();
+        double[] ball_yInts = ball.getYCoordinates();
 
-        if (y_points[0] <= 90) {
+        if (yInts[0] <= 90) {
             velY *= -1;
         }
-        if (y_points[3] >= 700) {
+        if (yInts[3] >= 700) {
             velY *= -1;
         }
 
         // if the ball hits the right side
-        if (ball_x_points[0] < this.x_points[0] + WIDTH
-                && ball_x_points[0] > this.x_points[0] + WIDTH - 10
-                && ball_y_points[0] < this.y_points[0] + HEIGHT
-                && ball_y_points[0] > this.y_points[0]) {
+        if (ball_xInts[0] < this.xInts[0] + WIDTH
+                && ball_xInts[0] > this.xInts[0] + WIDTH - 10
+                && ball_yInts[0] < this.yInts[0] + HEIGHT
+                && ball_yInts[0] > this.yInts[0]) {
             hit = true;
-            ball_x_points[0] = this.x_points[0] + WIDTH;
-            ball_x_points[1] = this.x_points[1] + WIDTH;
-            ball_x_points[2] = this.x_points[2] + ball.getWidth();
-            ball_x_points[3] = this.x_points[3] + ball.getWidth();
-            ball.set_x_points(ball_x_points);
+            ball_xInts[0] = this.xInts[0] + WIDTH;
+            ball_xInts[1] = this.xInts[1] + WIDTH;
+            ball_xInts[2] = this.xInts[2] + ball.getWidth();
+            ball_xInts[3] = this.xInts[3] + ball.getWidth();
+            ball.setXCoordinates(ball_xInts);
             ball.setVelX(ball.getVelX() * -1);
             hit_right = true;
         }
         // if the ball hits the left side
-        else if (ball_x_points[2] > this.x_points[0]
-                && ball_x_points[2] < this.x_points[0] + 10
-                && ball_y_points[2] < this.y_points[0] + HEIGHT
-                && ball_y_points[2] > this.y_points[0]) {
+        else if (ball_xInts[2] > this.xInts[0]
+                && ball_xInts[2] < this.xInts[0] + 10
+                && ball_yInts[2] < this.yInts[0] + HEIGHT
+                && ball_yInts[2] > this.yInts[0]) {
             hit = true;
-            ball_x_points[0] = this.x_points[0];
-            ball_x_points[1] = this.x_points[1];
-            ball_x_points[2] = this.x_points[1] - ball.getWidth();
-            ball_x_points[3] = this.x_points[1] - ball.getWidth();
-            ball.set_x_points(ball_x_points);
+            ball_xInts[0] = this.xInts[0];
+            ball_xInts[1] = this.xInts[1];
+            ball_xInts[2] = this.xInts[1] - ball.getWidth();
+            ball_xInts[3] = this.xInts[1] - ball.getWidth();
+            ball.setXCoordinates(ball_xInts);
             ball.setVelX(ball.getVelX() * -1);
             hit_left = true;
         }
         // if the ball hits the bottom
-        else if (ball_x_points[0] > this.x_points[0]
-                && ball_x_points[0] < this.x_points[0] + WIDTH
-                && ball_y_points[0] < this.y_points[0] + HEIGHT
-                && ball_y_points[0] > this.y_points[0] + HEIGHT - 10) {
+        else if (ball_xInts[0] > this.xInts[0]
+                && ball_xInts[0] < this.xInts[0] + WIDTH
+                && ball_yInts[0] < this.yInts[0] + HEIGHT
+                && ball_yInts[0] > this.yInts[0] + HEIGHT - 10) {
             hit = true;
-            ball_y_points[0] = this.y_points[0] + HEIGHT;
-            ball_y_points[1] = this.y_points[0] + HEIGHT + ball.getHeight();
-            ball_y_points[2] = this.y_points[0] + HEIGHT + ball.getHeight();
-            ball_y_points[3] = this.y_points[0] + HEIGHT;
-            ball.set_y_points(ball_y_points);
+            ball_yInts[0] = this.yInts[0] + HEIGHT;
+            ball_yInts[1] = this.yInts[0] + HEIGHT + ball.getHeight();
+            ball_yInts[2] = this.yInts[0] + HEIGHT + ball.getHeight();
+            ball_yInts[3] = this.yInts[0] + HEIGHT;
+            ball.setYCoordinates(ball_yInts);
             ball.setVelY(ball.getVelY() * -1);
             hit_bottom = true;
         }
         // if the ball hits the top
-        else if (ball_x_points[0] > this.x_points[0]
-                && ball_x_points[0] < this.x_points[0] + WIDTH
-                && ball_y_points[2] > this.y_points[0]
-                && ball_y_points[2] < this.y_points[0] + 10) {
+        else if (ball_xInts[0] > this.xInts[0]
+                && ball_xInts[0] < this.xInts[0] + WIDTH
+                && ball_yInts[2] > this.yInts[0]
+                && ball_yInts[2] < this.yInts[0] + 10) {
             hit = true;
-            ball_y_points[0] = this.y_points[0] + ball.getHeight();
-            ball_y_points[1] = this.y_points[0];
-            ball_y_points[2] = this.y_points[0];
-            ball_y_points[3] = this.y_points[0] + ball.getHeight();
-            ball.set_y_points(ball_y_points);
+            ball_yInts[0] = this.yInts[0] + ball.getHeight();
+            ball_yInts[1] = this.yInts[0];
+            ball_yInts[2] = this.yInts[0];
+            ball_yInts[3] = this.yInts[0] + ball.getHeight();
+            ball.setYCoordinates(ball_yInts);
             ball.setVelY(ball.getVelY() * -1);
             hit_top = true;
         }
