49cfc47cf7b9a234f4cb8dabbcdc96a76358ee0a
Anthony Comanzo <33241429+acomanzo@users.noreply.github.com>
2020-03-27 19:45:23 -0400

    Update Timer.java
    Removed the attempt at pausing the timer when the user clicks pause. It didn't work perfectly and wasn't super important. I'd like to figure it out some day. Added Color field that determines what color to render the text.

--- Timer.java
+++ Timer.java
@@ -5,62 +5,48 @@ import java.util.TimerTask;
 
 /**
  * Represents a timer. Used for countdowns at the beginning of the game, after a goal is scored, and
- * once the player returns from the pause menu.
+ * once the player returns from the pause menu; and keeps track of the total time of the mode.
  */
 public class Timer {
 
     private long startTime;
     private long elapsedSeconds = 0;
-    private long menuStartStamp;
-    private long elapsedSecondsInMenu = 0;
-    private long totalSecondsInMenu = 0;
-
-    private boolean menuStartStampUpdated = false;
-    private boolean totalSecondsInMenuUpdated = false;
 
     private Game game;
-    private Paddle player1;
-    private Paddle player2;
+
+    private Color color;
+
+    // represents the mode during which this timer should be active
+    private String mode;
 
     static long countdown = 4;
 
-    public Timer(Game game, Paddle player1, Paddle player2) {
+    public Timer(Game game, String mode, Color color) {
         this.game = game;
-        this.player1 = player1;
-        this.player2 = player2;
         startTime = System.currentTimeMillis();
-        menuStartStamp = System.currentTimeMillis();
+        this.mode = mode;
+        this.color = color;
     }
 
     public void tick (boolean countdownStarted) {
-        long elapsedTime = 0;
-        elapsedTime = System.currentTimeMillis() - startTime;
-        elapsedSeconds = elapsedTime / 1000;
 
-        if (game.getState().equals("MAIN MENU")) {
-            totalSecondsInMenuUpdated = false;
-
-            if (menuStartStampUpdated == false) {
-                menuStartStamp = System.currentTimeMillis();
-                menuStartStampUpdated = true;
-            }
-
-            long elapsedTimeInMenu = System.currentTimeMillis() - menuStartStamp;
-            elapsedSecondsInMenu = elapsedTimeInMenu / 1000;
-        }
-
-        if (game.getState().equals("PLAY")) {
-            menuStartStampUpdated = false;
-            if (totalSecondsInMenuUpdated == false) {
-                totalSecondsInMenu += elapsedSecondsInMenu;
-                totalSecondsInMenuUpdated = true;
-            }
-        }
-
-        if(countdownStarted == false && game.getState().equals("PLAY")) {
+        if(!countdownStarted && game.getState().equals(mode)) {
             countdown();
         }
-        //countdown(countdownStarted);
+
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        elapsedSeconds = (elapsedTime / 1000);
+
+        if (!game.getState().equals(mode)) {
+            pause();
+        }
+    }
+
+    /**
+     * Pauses the timer.
+     */
+    public void pause() {
+        // TODO
     }
 
     /**
@@ -73,8 +59,8 @@ public class Timer {
             @Override
             public void run() {
                 countdown--;
-                // this if block MUST be inside the run method!!! otherwise it won't cancel!!!
                 if(countdown <= 0) {
+                    game.setPauseCrazyModeBall(false);
                     t.cancel();
                     t.purge();
                 }
@@ -83,17 +69,21 @@ public class Timer {
     }
 
     public void render(Graphics g) {
-        g.setColor(Color.WHITE);
+        g.setColor(color);
         g.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-        g.drawString("Time: " + (elapsedSeconds - totalSecondsInMenu), 15, 25);
+        g.drawString("Time: " + (elapsedSeconds), 15, 25);
 
         g.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
 
-        if(countdown == 0) {
+        if (countdown == 0) {
             g.drawString("GO!", 500, 50);
         }
-        else
+        else if (countdown > 0) {
             g.drawString(countdown + "", 500, 50);
+        }
+        else {
+            g.drawString(countdown + "Error in timer", 500, 50);
+        }
 
     }
 
